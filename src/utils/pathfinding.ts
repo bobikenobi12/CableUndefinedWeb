c2d1b7a9b1e84a6fbcbc23aa1580f0b35b371868
ProGamer2711 <alexandurshestakov@gmail.com>
2024-04-16 11:11:39 +0300

    commented out the examples

--- src/utils/pathfinding.ts
+++ src/utils/pathfinding.ts
@@ -559,52 +559,52 @@ export function resetGraph() {
 }
 
 // ? Examples
-const path1 = addConnection(["MCUH0", "MAINH11"]);
-const path2 = addConnection(["MCUH1", "MAINH10"]);
-const path3 = addConnection(["MCUH0", "MAINH11"]);
+// const path1 = addConnection(["MCUH0", "MAINH11"]);
+// const path2 = addConnection(["MCUH1", "MAINH10"]);
+// const path3 = addConnection(["MCUH0", "MAINH11"]);
 
-console.log("Path Description:", path1.pathDescription);
-console.log("Connections:", path1.connections.join("   "));
+// console.log("Path Description:", path1.pathDescription);
+// console.log("Connections:", path1.connections.join("   "));
 
-console.log("Path Description:", path2.pathDescription);
-console.log("Connections:", path2.connections.join("   "));
+// console.log("Path Description:", path2.pathDescription);
+// console.log("Connections:", path2.connections.join("   "));
 
-console.log("Path Description:", path3.pathDescription);
-console.log("Connections:", path3.connections.join("   "));
+// console.log("Path Description:", path3.pathDescription);
+// console.log("Connections:", path3.connections.join("   "));
 
-const removedPath1 = removeConnection(["MCUH0", "MAINH11"]);
-console.log("Path Description:", removedPath1.pathDescription);
-console.log("Connections:", removedPath1.connections.join("\n"));
-console.log("Removed connection from MCUH0 to MAINH11");
+// const removedPath1 = removeConnection(["MCUH0", "MAINH11"]);
+// console.log("Path Description:", removedPath1.pathDescription);
+// console.log("Connections:", removedPath1.connections.join("\n"));
+// console.log("Removed connection from MCUH0 to MAINH11");
 
-const path4 = addConnection(["MCUH0", "MAINH11"]);
-console.log("Path Description:", path4.pathDescription);
-console.log("Connections:", path4.connections.join("\n"));
+// const path4 = addConnection(["MCUH0", "MAINH11"]);
+// console.log("Path Description:", path4.pathDescription);
+// console.log("Connections:", path4.connections.join("\n"));
 
-const path5 = addConnection(["MCUH1", "MAINH10"]);
-console.log("Path Description:", path5.pathDescription);
-console.log("Connections:", path5.connections.join("\n"));
+// const path5 = addConnection(["MCUH1", "MAINH10"]);
+// console.log("Path Description:", path5.pathDescription);
+// console.log("Connections:", path5.connections.join("\n"));
 
-const removedPath2 = removeConnection(["MCUH1", "MAINH10"]);
-console.log("Path Description:", removedPath2.pathDescription);
-console.log("Connections:", removedPath2.connections.join("\n"));
-console.log("Removed connection from MCUH1 to MAINH10");
+// const removedPath2 = removeConnection(["MCUH1", "MAINH10"]);
+// console.log("Path Description:", removedPath2.pathDescription);
+// console.log("Connections:", removedPath2.connections.join("\n"));
+// console.log("Removed connection from MCUH1 to MAINH10");
 
-const path6 = addConnection(["MCUH1", "MAINH10"]);
-console.log("Path Description:", path6.pathDescription);
-console.log("Connections:", path6.connections.join("\n"));
+// const path6 = addConnection(["MCUH1", "MAINH10"]);
+// console.log("Path Description:", path6.pathDescription);
+// console.log("Connections:", path6.connections.join("\n"));
 
-resetGraph();
-console.log("Reset Graph!");
+// resetGraph();
+// console.log("Reset Graph!");
 
-const path7 = addConnection(["MCUH1", "MAINH10"]);
-console.log("Path Description:", path7.pathDescription);
-console.log("Connections:", path7.connections.join("\n"));
+// const path7 = addConnection(["MCUH1", "MAINH10"]);
+// console.log("Path Description:", path7.pathDescription);
+// console.log("Connections:", path7.connections.join("\n"));
 
-const path8 = addConnection(["MCUH1", "MAINH10"]);
-console.log("Path Description:", path8.pathDescription);
-console.log("Connections:", path8.connections.join("\n"));
+// const path8 = addConnection(["MCUH1", "MAINH10"]);
+// console.log("Path Description:", path8.pathDescription);
+// console.log("Connections:", path8.connections.join("\n"));
 
-const path9 = addConnection(["MCUH0", "MAINH11"]);
-console.log("Path Description:", path9.pathDescription);
-console.log("Connections:", path9.connections.join("\n"));
+// const path9 = addConnection(["MCUH0", "MAINH11"]);
+// console.log("Path Description:", path9.pathDescription);
+// console.log("Connections:", path9.connections.join("\n"));
